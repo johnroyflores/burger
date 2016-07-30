78b2c8067fd77102019f94ce022aaa99098ac4ba
John <johnroyflores@gmail.com>
2016-07-30 16:57:04 -0500

    server.js changes

--- server.js
+++ server.js
@@ -1,56 +1,26 @@
-// *********************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-// *********************************************************************************
-
-
-/* TODO:
- *
- *	STEPS TO SEQUELIZE THE STAR WARS APP.
- *	1. Delete the orm from config. In your app folder, create a model folder
- *     with a character.js file in the model
- *	2. In character.js, model out the character table, as detailed
- *		 in the schema.sql file located in the root of this project directory.
- *	3. Remove all references to the old orm file, 
- *     and replace it with character.js
- *	4. Use Sequlize methods in place of the orm calls 
- *     to retrieve and insert data.
- *
- * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
-
-
-// Dependencies
-// =============================================================
-var express 	= require('express');
-var bodyParser 	= require('body-parser');
-var methodOverride = require('method-override')
-
-
-// Sets up the Express App
-// =============================================================
+var express = require('express');
+var methodOverride = require('method-override');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var routes = require('./controllers/burgers_controller.js');
 var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.static(process.cwd()+ '/public'));
 
-// static directory
-app.use(express.static('app/public'));
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
 
+app.use(methodOverride('_method'));
 
-// Routes
-// =============================================================
-
-require("./routes/api-routes.js")(app)
-require("./routes/html-routes.js")(app)
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
 
+app.use('/', routes);
 
-
-// Starts the server to begin listening 
-// =============================================================
-app.listen(PORT, function(){
-	console.log('App listening on PORT ' + PORT);
-})
+app.listen(process.env.PORT || 3000, function(){
+	console.log('listening');
+});
